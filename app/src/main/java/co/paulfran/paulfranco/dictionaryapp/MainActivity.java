d6316d4dc8a91efebc8c7060ca7ddea1b44dec33
Paul Franco <paulfranco@me.com>
2018-12-17 15:24:30 -0800

    Added Menu

--- app/src/main/java/co/paulfran/paulfranco/dictionaryapp/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/dictionaryapp/MainActivity.java
@@ -2,6 +2,8 @@ package co.paulfran.paulfranco.dictionaryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu_main; This adds items to the action bar if it is present
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/up button
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if (id == R.id.action_exit) {
+            System.exit(0);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
