13890bc5923446bd5a32856c2c30f8f5c2bb7105
Paul Franco <paulfranco@me.com>
2018-12-17 16:04:56 -0800

    Added SearchBar

--- app/src/main/java/co/paulfran/paulfranco/dictionaryapp/MainActivity.java
+++ app/src/main/java/co/paulfran/paulfranco/dictionaryapp/MainActivity.java
@@ -2,15 +2,33 @@ package co.paulfran.paulfranco.dictionaryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.SearchView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    SearchView search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        search = (SearchView) findViewById(R.id.search_view);
+
+        search.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                search.setIconified(false);
+            }
+        });
     }
 
     @Override
