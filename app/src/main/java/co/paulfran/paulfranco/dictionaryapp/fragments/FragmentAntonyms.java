f7a4d384c27fae991a995a0622de39275d0612d1
Paul Franco <paulfranco@me.com>
2018-12-19 23:30:52 -0800

    Added Fragments

---
+++ app/src/main/java/co/paulfran/paulfranco/dictionaryapp/fragments/FragmentAntonyms.java
@@ -0,0 +1,27 @@
+package co.paulfran.paulfranco.dictionaryapp.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.paulfran.paulfranco.dictionaryapp.R;
+
+public class FragmentAntonyms extends Fragment {
+
+    // Required Empty Contructor
+    public FragmentAntonyms() {
+
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_definition, container, false);
+        return view;
+    }
+
+
+
+
+}
