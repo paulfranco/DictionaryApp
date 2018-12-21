861cc90b6c8167bc374f06995685972ff77e73e6
Paul Franco <paulfranco@me.com>
2018-12-21 02:57:02 -0800

    Added english dictionary database

---
+++ app/src/main/java/co/paulfran/paulfranco/dictionaryapp/DatabaseHelper.java
@@ -0,0 +1,22 @@
+package co.paulfran.paulfranco.dictionaryapp;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private String DB_PATH = null;
+    private static String DB_NAME = "eng.dictionary.db";
+    private SQLiteDatabase myDatabase;
+    private final Context myContext;
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+        this.DB_PATH = "/data/data/" + context.getPackageName() + "/" + "databases/";
+        Log.e("Path 1", DB_PATH);
+    }
+}
