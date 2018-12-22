ed4d8ff70e352deef619941aaf801bcebae35c3b
Paul Franco <paulfranco@me.com>
2018-12-21 19:39:58 -0800

    updated DatabaseHelper

--- app/src/main/java/co/paulfran/paulfranco/dictionaryapp/DatabaseHelper.java
+++ app/src/main/java/co/paulfran/paulfranco/dictionaryapp/DatabaseHelper.java
@@ -1,9 +1,15 @@
 package co.paulfran.paulfranco.dictionaryapp;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 
 public class DatabaseHelper extends SQLiteOpenHelper {
@@ -18,5 +24,79 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         this.myContext = context;
         this.DB_PATH = "/data/data/" + context.getPackageName() + "/" + "databases/";
         Log.e("Path 1", DB_PATH);
+    }
+
+    public void createDataBase() throws IOException {
+        boolean dbExist = checkDatabase();
+
+        if (!dbExist) {
+            this.getReadableDatabase();
+            try {
+                copyDatabase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+
+    public boolean checkDatabase() {
+        SQLiteDatabase checkDB = null;
+        try {
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+        } catch (SQLException e) {
+            //
+        }
+        // if the database is not null
+        if (checkDB != null) {
+            checkDB.close();
+        }
+        return checkDB != null ? true : false;
+
+    }
+
+    private void copyDatabase() throws IOException {
+
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+        String outFileName = DB_PATH + DB_NAME;
+        OutputStream myOutput = new FileOutputStream(outFileName);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer)) > 0) {
+            myOutput.write(buffer, 0, length);
+        }
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+        Log.i("copyDatabase", "Database Copied");
+    }
+
+    public void openDataBase() throws SQLException {
+        String myPath = DB_PATH + DB_NAME;
+        myDatabase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READWRITE);
+    }
+
+    @Override
+    public synchronized void close() {
+        if (myDatabase != null) {
+            myDatabase.close();
+        }
+        super.close();
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        try {
+            this.getReadableDatabase();
+            myContext.deleteDatabase(DB_NAME);
+            copyDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
